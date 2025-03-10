1d0ee16be82ae363c27dac6e8fafacef374986a1
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-03-10 16:16:29 +0900

    기본 TDL TABLE 생성

---
+++ src/main/java/festival/dev/domain/TDL/entity/ToDoList.java
@@ -0,0 +1,26 @@
+package festival.dev.domain.TDL.entity;
+
+import festival.dev.global.entity.BaseTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "TDL")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToDoList extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Boolean completed;
+}
