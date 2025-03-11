d20585fd41fe428307b79d300f3e86b141caa878
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-03-11 15:43:01 +0900

    기본 TDL modify 기능 구현

--- src/main/java/festival/dev/domain/TDL/presentation/dto/request/InsertRequest.java
+++ src/main/java/festival/dev/domain/TDL/presentation/dto/request/InsertRequest.java
@@ -1,15 +1,12 @@
 package festival.dev.domain.TDL.presentation.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 
 @Getter
-public class ToDoListRequest {
+public class InsertRequest {
     @NonNull
     private String title;
-    @NonNull
-    private Boolean completed;
     @NonNull
     private String userID;
     @NonNull
