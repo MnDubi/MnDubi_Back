38100d144a5a2c264df40b1a8d39c296b2482285
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-04-08 10:45:50 +0900

    @authenticationprincipal 사용하게 변경

--- src/main/java/festival/dev/domain/calendar/presentation/CalendarController.java
+++ src/main/java/festival/dev/domain/calendar/presentation/CalendarController.java
@@ -1,13 +1,10 @@
 package festival.dev.domain.calendar.presentation;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import festival.dev.domain.calendar.service.CalendarService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import festival.dev.domain.user.entity.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,38 +14,38 @@ public class CalendarController {
 
     private final CalendarService calendarService;
 
-    @Value("${jwt.secret}")
-    private String secret;
+//    @Value("${jwt.secret}")
+//    private String secret;
 
     @GetMapping
-    public ResponseEntity<?> findDate(@RequestParam String date ,@RequestHeader String authorization){
+    public ResponseEntity<?> findDate(@RequestParam String date ,/*@RequestHeader String authorization*/@AuthenticationPrincipal CustomUserDetails user){
         try{
-            Long userID = getUserID(authorization);
-            return ResponseEntity.ok(calendarService.getDateCalendar(date, userID));
+//            Long userID = getUserID(authorization);
+            return ResponseEntity.ok(calendarService.getDateCalendar(date, /*userID*/user.getUserID()));
         }catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @GetMapping("/month")
-    public ResponseEntity<?> findMonth(@RequestHeader String authorization){
+    public ResponseEntity<?> findMonth(/*@RequestHeader String authorization*/@AuthenticationPrincipal CustomUserDetails user){
         try{
-            Long userID = getUserID(authorization);
-            return ResponseEntity.ok(calendarService.getByMonth(userID));
+//            Long userID = getUserID(authorization);
+            return ResponseEntity.ok(calendarService.getByMonth(/*userID*/user.getUserID()));
         }
         catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
-    public Long getUserID(String auth) {
-        String token = auth.replace("Bearer ", "");
-
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(secret.getBytes())
-                .build()
-                .parseClaimsJws(token).getBody();
-
-        return claims.get("userId", Long.class);
-    }
+//    public Long getUserID(String auth) {
+//        String token = auth.replace("Bearer ", "");
+//
+//        Claims claims = Jwts.parserBuilder()
+//                .setSigningKey(secret.getBytes())
+//                .build()
+//                .parseClaimsJws(token).getBody();
+//
+//        return claims.get("userId", Long.class);
+//    }
 }
