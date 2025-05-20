7e71b48ab93b015830f5beead7e20e9a00bf2c8b
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-05-20 19:57:04 +0900

    SSE 생성 Websocket을 대체할 신기술

--- src/main/java/festival/dev/domain/gorupTDL/presentation/GroupController.java
+++ src/main/java/festival/dev/domain/gorupTDL/presentation/GroupController.java
@@ -10,12 +10,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequestMapping("group/toDoList")
 @RequiredArgsConstructor
 public class GroupController {
-
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+    private final Map<String, SseEmitter> emitterMap = new ConcurrentHashMap<>();
     private final GroupService groupService;
 
     @PostMapping("/invite")
@@ -140,5 +148,24 @@ public class GroupController {
         catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/sse")
+    public SseEmitter connect() {
+        SseEmitter emitter = new SseEmitter();
+
+        emitters.add(emitter);
+
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitter.onError((e) -> emitters.remove(emitter));
+
+        try {
+            emitter.send(SseEmitter.event().name("connected").data("SSE 연결됨"));
+        } catch (IOException e) {
+            emitters.remove(emitter);
+        }
+
+        return emitter;
     }
 }
